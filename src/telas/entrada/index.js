1dc8980f06990fa438a1cd85cf1e19b6a32d2871
Maria Julia <mariajuliasantosbarbosa40@gmail.com>
2025-08-22 11:37:34 -0300

    dia22-08

--- src/telas/entrada/index.js
+++ src/telas/entrada/index.js
@@ -3,10 +3,10 @@ import {
   View,
   Text,
   Image,
-  Pressable,
-  Select,
+  Button,
   ImageBackground,
 } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
 import logo from '../../../assets/logo.png'
 import fundo from '../../../assets/fundomobile.png'
@@ -15,6 +15,7 @@ import fundo from '../../../assets/fundomobile.png'
 import styles from './styles';
 
 export default function Entrada() {
+  const [selectedUsuario, setSelectedUsuario] = useState();
 
   return (
     // <View style={styles.container}>
@@ -22,14 +23,27 @@ export default function Entrada() {
       <View style={styles.containerLogo}>
         <Image style={styles.logo} source={logo} />
         <Text style={styles.txtLogo}>BLUVA</Text>
-      </View>
 
+        
+        <Picker style={styles.pickerWrapper}
+  selectedValue={selectedUsuario}
+  onValueChange={(itemValue, itemIndex) =>
+    setSelectedUsuario(itemValue)
+  }>
+  <Picker.Item label="Cliente" value="Cliente" />
+  <Picker.Item label="Tecnico" value="Tecnico" />
+</Picker>
+      
       {/* <select id="tipoUsuario">
         <option value="">Selecione</option>
         <option value="usuario">Usuário</option>
         <option value="tecnico">Técnico</option>
       </select> */}
-      {/* <button onclick="window.location.href='../login/login.html'">Prosseguir</button> */}
+      <Button  style={styles.button} color={'#5a5b65'} title ="Entrar"
+          onPress={() => 0}
+        /> 
+      </View>
+        
     </ImageBackground>
     // </View>
   );
